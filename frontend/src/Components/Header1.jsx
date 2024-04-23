493f57abede3de2c3ccaea5a67feb3360fb058fc
Ganesh Kumar <mrganeshofficial8@gmail.com>
2024-04-23 08:55:48 +0530

    header1 responsiveness done

--- frontend/src/Components/Header1.jsx
+++ frontend/src/Components/Header1.jsx
@@ -6,7 +6,7 @@ export const Header = () => {
   return (
     <>
       <section>
-        <Box marginTop={"50px"}>
+        <Box marginTop={"70px"} paddingTop={"30px"} paddingBottom={"30px"}>
           <Box
             w={{
               base: "250px",
@@ -19,29 +19,46 @@ export const Header = () => {
             textAlign={"center"}
             marginTop={"20px"}
           >
-            <Heading as="h4" lineHeight={"50px"}>
+            <Heading as="h4" lineHeight={"40px"}>
               Design System for React JS
             </Heading>
-            <Text lineHeight={"25px"} color={"#505256"}>
+            <Text lineHeight={"25px"} color={"#505256"} marginTop={"10px"}>
               Automize React is a UI framework that helps developers collaborate
               with designers and build consistent userUnterfaces effortlessly.
             </Text>
           </Box>
           <Box
             display={"flex"}
+            flexDirection={{
+              base: "column",
+              sm: "column",
+              md: "row",
+              lg: "row",
+              xl: "row",
+            }}
             gap={"30px"}
             justifyContent={"center"}
             alignItems={"center"}
             marginTop={"30px"}
           >
-            <Button bg={"#0284FE"} color={"white"} _hover={"none"}>
+            <Button
+              bg={"#0284FE"}
+              color={"white"}
+              _hover={"none"}
+              width={{ base: "80%", sm: "80%", md: "15%", lg: "15%" }}
+              paddingTop={"25px"}
+              paddingBottom={"25px"}
+            >
               Get Started Now
             </Button>
             <Button
               bg={"white"}
               _hover={"none"}
+              width={{ base: "80%", sm: "80%", md: "15%", lg: "15%" }}
               border={"1px solid #505256 "}
               gap={"5px"}
+              paddingTop={"25px"}
+              paddingBottom={"25px"}
             >
               <IoPlaySharp />
               Watch video
